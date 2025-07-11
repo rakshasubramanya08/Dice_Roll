a12bb7096879527e9d100ffcd63b45914ae8fce7
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-11 20:09:21 +0200

    Added variable and 1 function.

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let diceV = document.getElementById('dice');
 let btnV = document.getElementById('btnCl');
 let rollV = document.getElementById('rollDice');
 
+btnV.addEventListener('click',()=>{
+
+})
+
+let DiceList = [];
+
+function diceRoll() {
+  const rollResult = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollResult);
+  diceV.innerHTML = diceFace;
+  List.push(rollResult);
+  updateRollHistory();
+}
+
+
