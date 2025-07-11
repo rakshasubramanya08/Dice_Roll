995cb18b375b4b065fdb36387eb8228d38d9ac47
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-11 20:22:38 +0200

    Added new functions and switch statements and add event listener to button roll dice.

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let btnV = document.getElementById('btnCl');
 let rollV = document.getElementById('rollDice');
 
 btnV.addEventListener('click',()=>{
+    diceV.classList.add("roll-animation");
+  setTimeout(() => {
+     diceV.classList.remove("roll-animation");
+    diceRoll();
+  }, 1000);
+});
 
-})
 
 let diceList = [];
 
@@ -18,8 +23,31 @@ function diceRoll() {
 
 function updateRollHistory() {
   rollV.innerHTML = "";
-  for (let i = 0; i < diceList.length; i++) {
-
-
+  for (let i = 0; i < diceList.length; i++){
+   const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      diceList[i]
+    )}</span>`;
+    rollV.appendChild(listItem);
 }
 }
+
+function getDiceFace(rollResult) {
+  switch (rollResult) {
+    case 1:
+      return "&#9856;";
+    case 2:
+      return "&#9857;";
+    case 3:
+      return "&#9858;";
+    case 4:
+      return "&#9859;";
+    case 5:
+      return "&#9860;";
+    case 6:
+      return "&#9861;";
+    default:
+      return "";
+  }
+}
+
